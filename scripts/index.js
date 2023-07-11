56266cff9ec5deb96d5b066176789846505baf78
Danil <85486945+lugdan@users.noreply.github.com>
2023-07-11 18:53:19 +0600

    first fixes

--- scripts/index.js
+++ scripts/index.js
@@ -6,39 +6,33 @@ const popupCloseButton = document.querySelector(".popup__close-button");
 const popupSubmitFormButton = document.querySelector(".popup__submit");
 
 function popupClassToggle(event) {
-  popup.classList.toggle("popup_open");
+  popup.classList.toggle("popup_openeded");
 
-  if (popup.classList.contains("popup_open")) {
-
+  if (popup.classList.contains("popup_openeded")) {
     const personName = document.querySelector(
       ".profile__item-heading-title"
-    ).innerHTML;
+    ).textContent;
     const personProfile = document.querySelector(
       ".profile__item-heading-subtitle"
-    ).innerHTML;
-  
+    ).textContent;
+
     document.querySelector(".popup__input_name").value = personName;
     document.querySelector(".popup__input_activity").value = personProfile;
-
-  } 
+  }
 
   if (event.currentTarget == popupSubmitFormButton) {
-    document.querySelector(
-      ".profile__item-heading-title"
-    ).innerHTML = document.querySelector(".popup__input_name").value;
- document.querySelector(
-    ".profile__item-heading-subtitle"
-    ).innerHTML = document.querySelector(".popup__input_activity").value;
+    document.querySelector(".profile__item-heading-title").textContent =
+      document.querySelector(".popup__input_name").value;
+    document.querySelector(".profile__item-heading-subtitle").textContent =
+      document.querySelector(".popup__input_activity").value;
   }
 }
 
 const closeModalByClickToOverlay = function (event) {
-
-  if (event.target == event.currentTarget){
-    popupClassToggle()
+  if (event.target == event.currentTarget) {
+    popupClassToggle();
   }
-  
-}
+};
 
 popupOpenButton.addEventListener("click", popupClassToggle);
 popupCloseButton.addEventListener("click", popupClassToggle);
